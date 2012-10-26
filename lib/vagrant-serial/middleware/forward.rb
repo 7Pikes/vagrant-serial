2ead96826293b02daa9915a3a5c27cffd6c33d5d
Anton Mironov <ant.mironov@gmail.com>
2012-10-26 21:50:52 +0400

    Fixed the execution of commands inside virtual machine

--- lib/vagrant-serial/middleware/forward.rb
+++ lib/vagrant-serial/middleware/forward.rb
@@ -7,8 +7,8 @@ module Vagrant
         end
 
         def call(env)
-          `socat tcp-l:30001,reuseaddr,fork UNIX-CONNECT:$HOME/serial/#{env[:machine].id}-com1 &`
-          `socat tcp-l:30002,reuseaddr,fork UNIX-CONNECT:$HOME/serial/#{env[:machine].id}-com2 &`
+          env[:vm].channel.execute("socat tcp-l:30001,reuseaddr,fork UNIX-CONNECT:$HOME/serial/#{env[:vm].id}-com1 &")
+          env[:vm].channel.execute("socat tcp-l:30002,reuseaddr,fork UNIX-CONNECT:$HOME/serial/#{env[:vm].id}-com2 &")
           @app.call(env)
         end
       end
