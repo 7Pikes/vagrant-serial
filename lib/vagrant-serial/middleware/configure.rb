b66625a942fb7badd3803d58ff495ca7ccd16395
Anton Mironov <ant.mironov@gmail.com>
2012-10-26 21:52:09 +0400

    Fixed configure step

--- lib/vagrant-serial/middleware/configure.rb
+++ lib/vagrant-serial/middleware/configure.rb
@@ -7,10 +7,10 @@ module Vagrant
         end
 
         def call(env)
-          command = ["modifyvm", env[:machine].id, "--uart1", "0x3F8", "4", "--uartmode1", "server", "/home/vagrant/serial/#{env[:machine].id}-com1"]
-          env[:machine].provider.driver.execute_command(command)
-          command = ["modifyvm", env[:machine].id, "--uart2", "0x2F8", "3", "--uartmode1", "server", "/home/vagrant/serial/#{env[:machine].id}-com2"]
-          env[:machine].provider.driver.execute_command(command)
+          command = ["modifyvm", env[:vm].id, "--uart1", "0x3F8", "4", "--uartmode1", "server", "$HOME/serial/#{env[:vm].id}-com1"]
+          env[:vm].provider.driver.execute_command(command)
+          command = ["modifyvm", env[:vm].id, "--uart2", "0x2F8", "3", "--uartmode1", "server", "$HOME/serial/#{env[:vm].id}-com2"]
+          env[:vm].provider.driver.execute_command(command)
           @app.call(env)
         end
       end
