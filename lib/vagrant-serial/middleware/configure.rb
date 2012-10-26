15906f50861d01b6b043c88a3b8f91060f0a32e1
Anton Mironov <ant.mironov@gmail.com>
2012-10-26 22:37:41 +0400

    Fixed typo for 2 com-port Fixed HOME path

--- lib/vagrant-serial/middleware/configure.rb
+++ lib/vagrant-serial/middleware/configure.rb
@@ -7,9 +7,9 @@ module Vagrant
         end
 
         def call(env)
-          command = ["modifyvm", env[:vm].uuid, "--uart1", "0x3F8", "4", "--uartmode1", "server", "$HOME/serial/#{env[:vm].uuid}-com1"]
+          command = ["modifyvm", env[:vm].uuid, "--uart1", "0x3F8", "4", "--uartmode1", "server", "#{ENV['HOME']}/serial/#{env[:vm].uuid}-com1"]
           env[:vm].driver.execute_command(command)
-          command = ["modifyvm", env[:vm].uuid, "--uart2", "0x2F8", "3", "--uartmode1", "server", "$HOME/serial/#{env[:vm].uuid}-com2"]
+          command = ["modifyvm", env[:vm].uuid, "--uart2", "0x2F8", "3", "--uartmode2", "server", "#{ENV['HOME']}/serial/#{env[:vm].uuid}-com2"]
           env[:vm].driver.execute_command(command)
           @app.call(env)
         end
