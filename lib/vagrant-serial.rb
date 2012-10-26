84447f71841ea6e66e33d654c302980f1bfee6ad
Anton Mironov <ant.mironov@gmail.com>
2012-10-26 21:51:50 +0400

    Fixed middleware stack insertion

--- lib/vagrant-serial.rb
+++ lib/vagrant-serial.rb
@@ -4,5 +4,5 @@ require "vagrant-serial/version"
 require "vagrant-serial/middleware/configure"
 require "vagrant-serial/middleware/forward"
 
-Vagrant.actions[:provision].insert_after(Vagrant::Action::VM::Provision, Vagrant::Serial::Middleware::Configure)
-Vagrant.actions[:start].insert_after(Vagrant::Action::VM::Provision, Vagrant::Serial::Middleware::Forward)
+Vagrant.actions[:start].insert_after Vagrant::Action::VM::ShareFolders, Vagrant::Serial::Middleware::Configure
+Vagrant.actions[:start].use Vagrant::Serial::Middleware::Forward
