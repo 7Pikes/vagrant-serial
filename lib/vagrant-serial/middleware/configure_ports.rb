19f89d8b884d73e22479f6afb040f0f97468b8cd
Anton Mironov <ant.mironov@gmail.com>
2012-11-04 23:14:45 +0400

    Typo fix

--- lib/vagrant-serial/middleware/configure_ports.rb
+++ lib/vagrant-serial/middleware/configure_ports.rb
@@ -7,15 +7,15 @@ module Vagrant
         end
 
         def call(env)
-          FileUtils.mkdir_p(env[:vm].config.sockets_path) if !File.directory?(env[:vm].config.sockets_path)
+          FileUtils.mkdir_p(env[:vm].config.serial.sockets_path) if !File.directory?(env[:vm].config.serial.sockets_path)
 
           if env[:vm].config.serial.forward_com1
-            command = ["modifyvm", env[:vm].uuid, "--uart1", "0x3F8", "4", "--uartmode1", "server", "#{env[:vm].config.sockets_path}/#{env[:vm].uuid}-com1"]
+            command = ["modifyvm", env[:vm].uuid, "--uart1", "0x3F8", "4", "--uartmode1", "server", "#{env[:vm].config.serial.sockets_path}/#{env[:vm].uuid}-com1"]
             env[:vm].driver.execute_command(command)
           end
 
           if env[:vm].config.serial.forward_com2
-            command = ["modifyvm", env[:vm].uuid, "--uart2", "0x2F8", "3", "--uartmode2", "server", "#{env[:vm].config.sockets_path}/#{env[:vm].uuid}-com2"]
+            command = ["modifyvm", env[:vm].uuid, "--uart2", "0x2F8", "3", "--uartmode2", "server", "#{env[:vm].config.serial.sockets_path}/#{env[:vm].uuid}-com2"]
             env[:vm].driver.execute_command(command)
           end
 
